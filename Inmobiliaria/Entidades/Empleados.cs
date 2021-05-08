de9bebb1296d37db3592bdbb60d1ccea57762f6e
Agustín Ezequiel Alvarez <agusalvarez99@hotmail.com>
2021-05-08 12:35:55 -0300

    login funcionando
    login + menu principal

---
+++ Inmobiliaria/Entidades/Empleados.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria
+{
+    public class Empleados
+    {
+        private int Legajo;
+        private string Nombre;
+        private string Apellido;
+        private string Calle;
+        private int NroCalle;
+        private int IdBarrio;
+        private int IdUsuario;
+        private string NombreUsuario;
+        private string Contraseña;
+
+        public int LegajoEmpleado { get => Legajo; set => Legajo = value; }
+        public string NombreEmpleado { get => Nombre; set => Nombre = value; }
+        public string ApellidoEmpleado { get => Apellido; set => Apellido = value; }
+        public string CalleEmpleado { get => Calle; set => Calle = value; }
+        public int NroCalleEmpleado { get => NroCalle; set => NroCalle = value; }
+        public int IdBarrioEmpleado { get => IdBarrio; set => IdBarrio = value; }
+        public int IdUsuarioEmpleado { get => IdUsuario; set => IdUsuario = value; }
+        public string NombreUsuarioEmpleado { get => NombreUsuario; set => NombreUsuario = value; }
+        public string ContraseñaEmpleado { get => Contraseña; set => Contraseña = value; }
+    }
+}
---
+++ Inmobiliaria/frmPrincipal.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Inmobiliaria
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vistasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propiedadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registrarEliminarEmpleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarEmpleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registrarEliminarClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registrarEliminarPropiedadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarPropiedadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.vistasToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empleadosToolStripMenuItem,
+            this.clientesToolStripMenuItem,
+            this.propiedadesToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // vistasToolStripMenuItem
+            // 
+            this.vistasToolStripMenuItem.Name = "vistasToolStripMenuItem";
+            this.vistasToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.vistasToolStripMenuItem.Text = "Vistas";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // empleadosToolStripMenuItem
+            // 
+            this.empleadosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrarEliminarEmpleadoToolStripMenuItem,
+            this.modificarEmpleadoToolStripMenuItem});
+            this.empleadosToolStripMenuItem.Name = "empleadosToolStripMenuItem";
+            this.empleadosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.empleadosToolStripMenuItem.Text = "Empleados";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrarEliminarClienteToolStripMenuItem,
+            this.modificarClienteToolStripMenuItem});
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // propiedadesToolStripMenuItem
+            // 
+            this.propiedadesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrarEliminarPropiedadesToolStripMenuItem,
+            this.modificarPropiedadToolStripMenuItem});
+            this.propiedadesToolStripMenuItem.Name = "propiedadesToolStripMenuItem";
+            this.propiedadesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.propiedadesToolStripMenuItem.Text = "Propiedades";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // registrarEliminarEmpleadoToolStripMenuItem
+            // 
+            this.registrarEliminarEmpleadoToolStripMenuItem.Name = "registrarEliminarEmpleadoToolStripMenuItem";
+            this.registrarEliminarEmpleadoToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.registrarEliminarEmpleadoToolStripMenuItem.Text = "Registrar/Eliminar Empleado";
+            // 
+            // modificarEmpleadoToolStripMenuItem
+            // 
+            this.modificarEmpleadoToolStripMenuItem.Name = "modificarEmpleadoToolStripMenuItem";
+            this.modificarEmpleadoToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.modificarEmpleadoToolStripMenuItem.Text = "Modificar Empleado";
+            // 
+            // registrarEliminarClienteToolStripMenuItem
+            // 
+            this.registrarEliminarClienteToolStripMenuItem.Name = "registrarEliminarClienteToolStripMenuItem";
+            this.registrarEliminarClienteToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.registrarEliminarClienteToolStripMenuItem.Text = "Registrar/Eliminar Cliente";
+            // 
+            // modificarClienteToolStripMenuItem
+            // 
+            this.modificarClienteToolStripMenuItem.Name = "modificarClienteToolStripMenuItem";
+            this.modificarClienteToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.modificarClienteToolStripMenuItem.Text = "Modificar Cliente";
+            // 
+            // registrarEliminarPropiedadesToolStripMenuItem
+            // 
+            this.registrarEliminarPropiedadesToolStripMenuItem.Name = "registrarEliminarPropiedadesToolStripMenuItem";
+            this.registrarEliminarPropiedadesToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.registrarEliminarPropiedadesToolStripMenuItem.Text = "Registrar/Eliminar Propiedad";
+            // 
+            // modificarPropiedadToolStripMenuItem
+            // 
+            this.modificarPropiedadToolStripMenuItem.Name = "modificarPropiedadToolStripMenuItem";
+            this.modificarPropiedadToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.modificarPropiedadToolStripMenuItem.Text = "Modificar Propiedad";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registrarEliminarEmpleadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarEmpleadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registrarEliminarClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propiedadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registrarEliminarPropiedadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarPropiedadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vistasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+    }
+}
