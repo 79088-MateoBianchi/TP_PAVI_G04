fcb6d92635cea171b79fa8a702081d8469e5e3aa
JoaquinTissera <tisserajoaquin@gmail.com>
2021-05-11 14:52:06 -0300

    Entidades y AD creado

---
+++ Inmobiliaria/AccesoADatos/AD_Clientes.cs
@@ -0,0 +1,186 @@
+﻿using Inmobiliaria.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria.AccesoADatos
+{
+    public class AD_Clientes
+    {
+        public static DataTable ObtenerListadoClientes()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT nro_documento_c,tipo_documento_c, n_cliente, apellido_cliente,tel_cliente, calle, nro_calle, id_barrio FROM clientes";
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        public static bool AgregarClienteBD(Cliente cl)
+        {
+            bool res = false;
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "INSERT INTO clientes(nro_documento_c, tipo_documento_c, n_cliente, apellido_cliente, tel_cliente, calle, nro_calle, id_barrio) VALUES (@documentoCliente, @tipoDocumento, @nombreCliente,@apellidoCliente,@telefonoCliente @Calle, @numeroCalle, @idBarrio)";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@documentoCliente", cl.DocumentoCliente);
+                cmd.Parameters.AddWithValue("@tipoDocumento", cl.TipoDocumentoCliente);
+                cmd.Parameters.AddWithValue("@nombreCliente", cl.NombreCliente);
+                cmd.Parameters.AddWithValue("@apellidoCliente", cl.ApellidoCliente);
+                cmd.Parameters.AddWithValue("@telefonoCliente", cl.TelefonoCliente);
+                cmd.Parameters.AddWithValue("@Calle", cl.CalleCliente);
+                cmd.Parameters.AddWithValue("@numeroCalle", cl.NroCasaCliente);
+                cmd.Parameters.AddWithValue("@idBarrio", cl.IdBarrio);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                res = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return res;
+        }
+
+
+        public static Cliente ObtenerCliente(int documento)
+        {
+            Cliente cl = new Cliente();
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT * FROM clietes where nro_documento_c like @documentoCliente";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@documentoCliente", documento);
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr != null && dr.Read())
+                {
+                    cl.DocumentoCliente = int.Parse(dr["nro_documento_c"].ToString());
+                    cl.TipoDocumentoCliente = int.Parse(dr["tipo_documento_c"].ToString());
+                    cl.NombreCliente = dr["n_cliente"].ToString();
+                    cl.ApellidoCliente = dr["apellido_cliente"].ToString();
+                    cl.TelefonoCliente = int.Parse(dr["tel_cliente"].ToString());
+
+                    cl.CalleCliente = dr["calle"].ToString();
+                    cl.NroCasaCliente = int.Parse(dr["nro_calle"].ToString());
+                    cl.IdBarrio = int.Parse(dr["id_barrio"].ToString());
+
+
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return cl;
+        }
+
+        public static bool ActualizarCliente(Cliente cl)
+        {
+
+            //UPDATE table_name
+            //SET column1 = value1, column2 = value2, ...
+            //WHERE condition;
+
+            bool res = false;
+
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "UPDATE clientes SET nro_documento_c = @documentoCliente, tipo_documento_c = @tipoDocumento, n_cliente = @nombreCliente,apellido_cliente = @apellidoCliente,tel_cliente = @telefonoCliente, calle = @Calle, nro_calle = @numeroCalle, id_barrio = @idBarrio WHERE nro_documento_c like @documentoCliente";
+                cmd.Parameters.Clear();
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@documentoCliente", cl.DocumentoCliente);
+                cmd.Parameters.AddWithValue("@tipoDocumento", cl.TipoDocumentoCliente);
+                cmd.Parameters.AddWithValue("@nombreCliente", cl.NombreCliente);
+                cmd.Parameters.AddWithValue("@apellidoCliente", cl.ApellidoCliente);
+                cmd.Parameters.AddWithValue("@telefonoCliente", cl.TelefonoCliente);
+                cmd.Parameters.AddWithValue("@Calle", cl.CalleCliente);
+                cmd.Parameters.AddWithValue("@numeroCalle", cl.NroCasaCliente);
+                cmd.Parameters.AddWithValue("@idBarrio", cl.IdBarrio);
+
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+            
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                res = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Inmobiliaria/Entidades/Cliente.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria.Entidades
+{
+    public class Cliente
+    {
+        int nro_documento_c;
+        int tipo_documento_c;
+        string n_cliente;
+        string apellido_cliente;
+        int tel_client;
+        string calle;
+        int nro_calle;
+        int id_barrio;
+
+        public int DocumentoCliente
+        {
+            get => nro_documento_c;
+            set => nro_documento_c = value;
+        }
+
+        public int TipoDocumentoCliente
+        {
+            get => tipo_documento_c;
+            set => tipo_documento_c = value;
+        }
+
+        public string NombreCliente
+        {
+            get => n_cliente;
+            set => n_cliente = value;
+        }
+
+        public string ApellidoCliente
+        {
+            get => apellido_cliente;
+            set => apellido_cliente = value;
+        }
+
+        public int TelefonoCliente
+        {
+            get => tel_client;
+            set => tel_client = value;
+        }
+
+        public string CalleCliente
+        {
+            get => calle;
+            set => calle = value;
+        }
+
+        public int NroCasaCliente
+        {
+            get => nro_calle;
+            set => nro_calle = value;
+        }
+
+        public int IdBarrio
+        {
+            get => id_barrio;
+            set => id_barrio = value;
+        }
+
+
+    }
+}
