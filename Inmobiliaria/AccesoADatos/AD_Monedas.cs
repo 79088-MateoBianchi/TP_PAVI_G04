cae916be04b3960a01d4ee8f61a77096b8079227
Rodrigo Virgolini <rodrigovirgolini@gmail.com>
2021-05-16 23:56:56 -0300

    ABM Monedas

--- Inmobiliaria/AccesoADatos/AD_Monedas.cs
+++ Inmobiliaria/AccesoADatos/AD_Monedas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inmobiliaria.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -10,21 +11,59 @@ namespace Inmobiliaria.AccesoADatos
 {
     public class AD_Monedas
     {
+        public static Moneda ObtenerMoneda(string id)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            Moneda mon = new Moneda();
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT * FROM monedas where id_moneda like @id_moneda";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@id_moneda", id);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr != null && dr.Read())
+                {
+                    mon.IdMoneda = int.Parse(dr["id_moneda"].ToString());
+                    mon.NombreMoneda = dr["n_moneda"].ToString();
+                    mon.CotizacionMoneda = int.Parse(dr["cotizacion_pesos_arg"].ToString());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return mon;
+
+        }
+
+
         public static DataTable ObtenerMonedas()
         {
             string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
             SqlConnection cn = new SqlConnection(cadenaConexion);
-
             try
             {
+
                 SqlCommand cmd = new SqlCommand();
 
-                string consulta = "GetMonedas";
+                string consulta = "SELECT * FROM monedas";
 
                 cmd.Parameters.Clear();
-                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandType = CommandType.Text;
                 cmd.CommandText = consulta;
-
 
                 cn.Open();
                 cmd.Connection = cn;
@@ -35,7 +74,37 @@ namespace Inmobiliaria.AccesoADatos
                 da.Fill(tabla);
 
                 return tabla;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
 
+        public static bool AgregarMonedaABD(Moneda mon)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            bool resultado = false;
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                //string consulta = "GetUsuario";
+                string consulta = "INSERT INTO monedas (n_moneda, cotizacion_pesos_arg) VALUES (@nombre, @cotizacion)";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@nombre", mon.NombreMoneda);
+                cmd.Parameters.AddWithValue("@cotizacion", mon.CotizacionMoneda);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                resultado = true;
             }
             catch (Exception)
             {
@@ -44,7 +113,89 @@ namespace Inmobiliaria.AccesoADatos
             finally
             {
                 cn.Close();
+            }
 
+            return resultado;
+
+        }
+
+
+        public static bool ValidarAlRegistrar(string nombre)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                bool resultado = false;
+                SqlCommand cmd = new SqlCommand();
+
+                string consulta = "SELECT * FROM monedas WHERE n_moneda like @nombreMoneda";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@nombreMoneda", nombre);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                if (tabla.Rows.Count == 1)
+                {
+                    resultado = true;
+                }
+                else
+                {
+                    resultado = false;
+                }
+
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+
+            }
+        }
+
+        public static DataTable ObtenerMonedaXNombre(string parametro)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand();
+
+                string consulta = "SELECT * FROM monedas where n_moneda = @nombreMoneda";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@nombreMoneda", parametro);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
             }
         }
     }
